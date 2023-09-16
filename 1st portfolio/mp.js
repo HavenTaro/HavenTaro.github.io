5c186019ebdaf63fdd600ba3760b6d299e15984f
Haven <130215444+HavenTaro@users.noreply.github.com>
2023-09-16 22:27:44 +0800

    Add files via upload

---
+++ 1st portfolio/mp.js
@@ -0,0 +1,18 @@
+const add = "+"
+const sub = "-"
+const division = "/"
+const multiplication = "*"
+
+function sum(sum){
+    if(sum === add){
+        console.log('addition')
+    }
+
+    else if(sum === sub) {
+        console.log('subtraction')
+    }
+
+    else {
+        console.log('none')
+    }
+}
